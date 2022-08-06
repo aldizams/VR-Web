0a9f3803ea2abbeb77c8b5686552a4b217a9f9b5
aldizams <mr.aldiza@gmail.com>
2022-08-07 06:38:43 +0700

    add login route

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Card, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+	const [validated, setValidated] = useState(false);
+
+	const handleSubmit = (event) => {
+		const form = event.currentTarget;
+		if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
+
+		setValidated(true);
+	};
 	return (
 		<div className="login">
 			<Card style={{ width: '20rem' }}>
 				<Card.Body>
 					<img src="https://sso.universitaspertamina.ac.id/images/logo.png" />
-					<Form style={{ paddingBottom: '26px' }}>
+					<Form
+						style={{ paddingBottom: '26px' }}
+						onSubmit={handleSubmit}
+						validated={validated}
+					>
 						<Form.Group className="mb-3" controlId="formBasicEmail">
 							<Form.Label>Email address</Form.Label>
-							<Form.Control type="email" placeholder="Enter email" />
+							<Form.Control type="email" placeholder="Enter email" required />
+							<Form.Control.Feedback type="invalid">
+								Email Address Kosong!
+							</Form.Control.Feedback>
 						</Form.Group>
 
 						<Form.Group className="mb-3" controlId="formBasicPassword">
 							<Form.Label>Password</Form.Label>
-							<Form.Control type="password" placeholder="Password" />
+							<Form.Control type="password" placeholder="Password" required />
+							<Form.Control.Feedback type="invalid">
+								Password Kosong!
+							</Form.Control.Feedback>
 						</Form.Group>
 
 						<Button
@@ -24,7 +45,7 @@ const Login = () => {
 							type="submit"
 							style={{ width: '18rem' }}
 							as={Link}
-							to=""
+							to={`/UPerVR/admin/dashboard`}
 						>
 							Login
 						</Button>
